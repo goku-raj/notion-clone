95700f99e2e01a1dd49d280ebf19884df1c059cf
goku-raj <anshuraj1479@gmail.com>
2023-10-25 23:24:59 +0530

    Schema & API

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -2,12 +2,29 @@
 
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
+
+import { api } from "@/convex/_generated/api";
+import { useMutation } from "convex/react";
+
 import { PlusCircleIcon } from "lucide-react";
 import Image from "next/image";
+import { toast } from "sonner";
 
 const DocumentsPage = () => {
-    const {user} = useUser();
-    return ( 
+    const { user } = useUser();
+    const create = useMutation(api.documents.create);
+
+    const onCreate = () => {
+        const promise = create({ title: "Untitled" });
+
+        toast.promise(promise, {
+            loading: "Creating a new note...",
+            success: "New note created!",
+            error: "Failed to create a new note."
+        });
+    };
+
+    return (
         <div className="h-full flex flex-col items-center justify-center space-y-4">
             <Image
                 src={"/empty.png"}
@@ -26,12 +43,12 @@ const DocumentsPage = () => {
             <h2 className="text-lg font-medium">
                 Welcome to {user?.firstName}&apos;s Jotion
             </h2>
-            <Button>
-                <PlusCircleIcon className="h-4 w-4 mr-2"/>
+            <Button onClick={onCreate}>
+                <PlusCircleIcon className="h-4 w-4 mr-2" />
                 Create a note!
             </Button>
         </div>
-     );
+    );
 }
- 
+
 export default DocumentsPage;
