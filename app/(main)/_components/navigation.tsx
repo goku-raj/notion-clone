f3ebef4fefcbf8fed4f1ddda8afc5de8aab3f3f1
goku-raj <anshuraj1479@gmail.com>
2023-11-02 23:13:42 +0530

    Search Functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -24,12 +24,14 @@ import {
     PopoverTrigger,
     PopoverContent
 } from "@/components/ui/popover";
+import { useSearch } from "@/hooks/use-search";
 
 import { toast } from "sonner";
 import { DocumentList } from "./document-list";
 import { TrashBox } from "./trash-box";
 
 export const Navigation = () => {
+    const search = useSearch();
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
     const create = useMutation(api.documents.create);
@@ -152,7 +154,7 @@ export const Navigation = () => {
                         label="Search"
                         icon={Search}
                         isSearch
-                        onClick={() => { }}
+                        onClick={search.onOpen}
                     />
                     <Item
                         label="Settings"
